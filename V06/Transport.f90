0a23d53da5b6931cc725445544735236c717632d
JosRfis <110145179+JosRfis@users.noreply.github.com>
2022-09-01 12:15:26 +0900

    Update Transport.f90

--- V06/Transport.f90
+++ V06/Transport.f90
@@ -59,7 +59,7 @@ CONTAINS
          do u = 1, Ndim
          do j = 1, Ndim
       curr (i) = curr (i) +    &
-            (rho (l,u,i)*GC(l,j,j,u)+conjg(rho (l,u,i))*conjg(GC(u,j,j,l))*  &
+            (rho (l,u,i)*GC(l,j,j,u)+conjg(rho (l,u,i))*conjg(GC(l,j,j,u))*  &
             (1+Pulse*((1-Electrode)*gamma_L_1/gamma_L_0+Electrode*gamma_R_1/gamma_R_0)))
          enddo
          enddo
